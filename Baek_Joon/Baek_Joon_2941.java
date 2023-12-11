88af90c2fcdd1d536914aa6d09cc604238d04e3f
Donghun Lee <programmer_h@naver.com>
2023-12-11 20:14:48 +0900

    11 Dec

--- Baek_Joon/Baek_Joon_2941.java
+++ Baek_Joon/Baek_Joon_2941.java
@@ -6,36 +6,10 @@ public class Baek_Joon_2941 {
         String input = br.readLine();
         String[] croatia = {"c=","c-","dz=","d-","lj","nj","s=","z="};
         
-        int word_count = 0;
         for(int i = 0; i < croatia.length; i++) {
-            int alphabet_size = input.length();
-            input = input.replaceAll(croatia[i], "");
-            
-            if(croatia[i].length() == 3) {
-                if(alphabet_size - input.length() == 3) {
-                    word_count++;
-                }
-                else if(alphabet_size - input.length() > 3) {
-                    word_count += (alphabet_size - input.length()) / 3;
-                }
-                else {
-                    continue;
-                }
-            }
-            else {
-               if(alphabet_size - input.length() == 2) {
-                    word_count++;
-                }
-                else if(alphabet_size - input.length() > 2) {
-                    word_count += (alphabet_size - input.length()) / 2;
-                }
-                else {
-                    continue;
-                } 
-            }
+            input = input.replaceAll(croatia[i], " ");            
         }
 
-        word_count += input.length();
-        System.out.println(word_count);
+        System.out.println(input.length());
     }
 }
