21b172e7ae0d76892170f869fe56fa17e791a47f
Donghun Lee <programmer_h@naver.com>
2024-04-18 23:27:33 +0900

    18 Apr

---
+++ Baek_Joon/Baek_Joon_17626.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_17626 {
+    static int min = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n+1];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++) {
+            dp[i] = dp[i-1];
+            for(int j = 1; j*j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i-j*j]);                
+            }
+            dp[i]++;
+        }
+        System.out.println(dp[n]);
+    }
+}
