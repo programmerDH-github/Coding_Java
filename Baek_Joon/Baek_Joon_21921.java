7d4b02b8a7336d0a418f4d6cc29e1c77cded19a3
Donghun Lee <programmer_h@naver.com>
2024-07-12 18:26:33 +0900

    12 Jul

---
+++ Baek_Joon/Baek_Joon_21921.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Baek_Joon_21921 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] N_X = br.readLine().split(" ");
+        int N = Integer.parseInt(N_X[0]);
+        int X = Integer.parseInt(N_X[1]);
+
+        int[] days = new int[N+1];
+        String[] day = br.readLine().split(" ");
+        for(int i = 1; i <= N; i++) {
+            days[i] = Integer.parseInt(day[i-1]);
+        }
+
+        int[] dp = new int[N+1];
+        for(int i = 1; i <= N; i++) {
+            dp[i] = dp[i-1] + days[i];
+        }
+
+        int max = 0;
+        ArrayList<Integer> arraylist = new ArrayList<>();
+        for(int i = 1; i <= N; i++) {
+            if(i+X-1 > N) break;
+            int visit = dp[i+X-1] - dp[i-1];
+            arraylist.add(visit);
+            if(max < visit) max = visit;
+        }
+
+        Collections.sort(arraylist, Collections.reverseOrder());
+
+        if(arraylist.get(0) == 0) System.out.println("SAD");
+        else {
+            System.out.println(arraylist.get(0));
+            int count = 1;
+            for(int i = 1; i < arraylist.size(); i++) {
+                if(arraylist.get(i) == arraylist.get(i-1)) {
+                    count++;
+                }
+                else break;
+            }    
+            System.out.println(count);
+        }
+
+    }
+}
