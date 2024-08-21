65df590cfa590b49ccba932803b4963c57ffbcf4
Donghun Lee <programmer_h@naver.com>
2024-08-21 23:58:53 +0900

    21 Aug

--- Baek_Joon/Baek_Joon_14719.java
+++ Baek_Joon/Baek_Joon_14719.java
@@ -8,16 +8,33 @@ public class Baek_Joon_14719 {
         String[] H_W = br.readLine().split(" ");
         int H = Integer.parseInt(H_W[0]);
         int W = Integer.parseInt(H_W[1]);
-
-        int maxH = 0;        
+     
         int[] map = new int[W];
         String[] block = br.readLine().split(" ");
         for(int i = 0; i < W; i++) {
             map[i] = Integer.parseInt(block[i]);
-            if(map[i] > maxH) maxH = map[i];
+            
         }
 
-        System.out.println(maxH);
+        int answer = 0;
+        for(int i = 1; i < W-1; i++) {
+            int leftH = 0;
+            int rightH = 0;
+
+            for(int j = 0; j < i; j++) {
+                leftH = Math.max(map[j], leftH);
+            }
+
+            for(int j = i+1; j < W; j++) {
+                rightH = Math.max(map[j], rightH);
+            }
+
+            if(map[i] < leftH && map[i] < rightH) answer += Math.min(leftH, rightH) - map[i]; 
+
+
+        }
+
+        System.out.println(answer);
 
     }
 }
