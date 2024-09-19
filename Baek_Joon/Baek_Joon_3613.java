9c2d4b55c49582666b89bb2bf667511326c135e8
Donghun Lee <programmer_h@naver.com>
2024-09-20 03:17:44 +0900

    20 Sep

--- Baek_Joon/Baek_Joon_3613.java
+++ Baek_Joon/Baek_Joon_3613.java
@@ -5,21 +5,51 @@ import java.io.InputStreamReader;
 public class Baek_Joon_3613 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split("_");
-
-        if(input.length == 1) {
-
-        }
-        else {
-            String answer = input[0];
-            for(int i = 1; i < input.length; i++) {
-                answer += input[i].substring(0,1).toUpperCase();
-                answer += input[i].substring(1);
-            }
-            System.out.println(answer);
-        }
-
+        String input = br.readLine();
         
+        if(input.contains("__") || input.charAt(0) == '_' || input.charAt(input.length()-1) == '_' || (input.charAt(0) >= 65 && input.charAt(0) <= 90)) {
+            System.out.println("Error!");
+            return;
+        }
+
+        String answer = "";
+        // C++
+        if(input.contains("_")) {
+            String[] input_arr = input.split("_");
+
+            for(int i = 0; i < input_arr.length; i++) {
+                for(int j = 0; j < input_arr[i].length(); j++) {
+                    if((int)input_arr[i].charAt(j) >= 65 && (int)input_arr[i].charAt(j) <= 90) {
+                        System.out.println("Error!");
+                        return;
+                    }
+
+                    if(j == 0 && i != 0) {
+                        answer += (input_arr[i].charAt(j)+"").toUpperCase();
+                    }
+                    else {
+                        answer += input_arr[i].charAt(j);
+                    }
+
+
+                }
+
+            }
+
+        }
+        // Java
+        else {
+            for(int i = 0; i < input.length(); i++) {
+                if((int)input.charAt(i) >= 65 && (int)input.charAt(i) <= 90) {
+                    answer += "_";
+                    answer += (input.charAt(i)+"").toLowerCase();
+                }   
+                else answer += input.charAt(i);
+            }
+        }
+
+
+        System.out.println(answer);
 
     }
 }
