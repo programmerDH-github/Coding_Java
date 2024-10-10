b76108b5b404c029cd5b4a190ce1267a0eccf8e7
Donghun Lee <programmer_h@naver.com>
2024-10-11 04:33:33 +0900

    11 Oct

--- Baek_Joon/Baek_Joon_11055.java
+++ Baek_Joon/Baek_Joon_11055.java
@@ -8,19 +8,27 @@ public class Baek_Joon_11055 {
         int N = Integer.parseInt(br.readLine());
 
         int[] A = new int[N];
+        String[] input = br.readLine().split(" ");
         for(int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(br.readLine());
+            A[i] = Integer.parseInt(input[i]);
         }
 
-        int[][] dp = new int[N][N];
+        int[] dp = new int[N];
         
         for(int i = 0; i < N; i++) {
-            for(int j = 0; j < N; j++) {
-                
+            dp[i] = A[i];
+            for(int j = 0; j < i; j++) {
+                if(A[i] > A[j]) dp[i] = Math.max(dp[i], dp[j]+A[i]);
             }
         }
 
+        int max = 0;
+        for(int i = 0; i < N; i++) {
+            System.out.print(dp[i] + " ");
+            if(max < dp[i]) max = dp[i];
+        }
 
+        // System.out.println(max);
 
     }
 }
