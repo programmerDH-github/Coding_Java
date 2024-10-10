baf7ceeba98d54024247465aacc78bf205b22283
Donghun Lee <programmer_h@naver.com>
2024-10-10 23:58:05 +0900

    10 Oct

---
+++ Baek_Joon/Baek_Joon_11055.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_11055 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        int[] A = new int[N];
+        for(int i = 0; i < N; i++) {
+            A[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[][] dp = new int[N][N];
+        
+        for(int i = 0; i < N; i++) {
+            for(int j = 0; j < N; j++) {
+                
+            }
+        }
+
+
+
+    }
+}
