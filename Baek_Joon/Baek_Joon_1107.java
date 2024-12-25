147d792b8cc5f1d4890e985ee8915e75b9c0fd25
Donghun Lee <programmer_h@naver.com>
2024-12-26 00:00:11 +0900

    26 Dec

--- Baek_Joon/Baek_Joon_1107.java
+++ Baek_Joon/Baek_Joon_1107.java
@@ -15,7 +15,57 @@ public class Baek_Joon_1107 {
             arraylist.add(Integer.parseInt(input[i]));
         }
 
-        System.out.println(arraylist);
-        System.out.println();
+        if(arraylist.size() == 10) {
+            System.out.println(Math.abs(Integer.parseInt(target) - 100));
+            return;
+        }
+        if(Integer.parseInt(target) == 100) {
+            System.out.println(0);
+            return;
+        }
+
+        ArrayList<Integer> up = new ArrayList<>();
+        ArrayList<Integer> down = new ArrayList<>();
+
+        for(int i = 0; i < target.length(); i++) {
+            for(int j = 0; j <= 10; j++) {
+                int newNumber = target.charAt(i) - '0' + j;
+                
+                if(newNumber > 9) newNumber -= 10;
+
+                if(!arraylist.contains(newNumber)) {
+                    up.add(newNumber);
+                    break;
+                }
+
+            }
+
+        }
+
+        for(int i = 0; i < target.length(); i++) {
+            for(int j = 0; j <= 10; j++) {
+                int newNumber = target.charAt(i) - '0' - j;
+                if(newNumber < 0) newNumber += 10;
+
+                if(!arraylist.contains(newNumber)) {
+                    down.add(newNumber);
+                    break;
+                }
+
+            }
+
+        }
+
+        int upNumber = 0;
+        int downNumber = 0;
+
+        for(int i = 0; i < up.size(); i++) {
+            upNumber += up.get(i) * Math.pow(10, up.size()-(i+1));
+            downNumber += down.get(i) * Math.pow(10, up.size()-(i+1));
+        }
+        int answer = Math.min(Math.abs(upNumber-Integer.parseInt(target)),Math.abs(downNumber-Integer.parseInt(target))) + target.length();
+        System.out.println(Math.min(Math.abs(100-Integer.parseInt(target)),answer));
+
+
     }
 }
