111504f1a5a36c1ff5f491778b72fe2806f59e24
Donghun Lee <programmer_h@naver.com>
2024-12-27 04:58:17 +0900

    27 Dec

--- Baek_Joon/Baek_Joon_1107.java
+++ Baek_Joon/Baek_Joon_1107.java
@@ -1,71 +1,45 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Baek_Joon_1107 {
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String target = br.readLine();
+        int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         
-        ArrayList<Integer> arraylist = new ArrayList<>();
+        boolean[] broken = new boolean[10];
+        int answer = Math.abs(100-N);
+
+        // if(M == 0) {
+        //     System.out.println(String.valueOf(N).length());
+        //     return;
+        // }
+
         String[] input = br.readLine().split(" ");
         for(int i = 0; i < M; i++) {
-            arraylist.add(Integer.parseInt(input[i]));
+            broken[Integer.parseInt(input[i])] = true;
         }
 
-        if(arraylist.size() == 10) {
-            System.out.println(Math.abs(Integer.parseInt(target) - 100));
-            return;
-        }
-        if(Integer.parseInt(target) == 100) {
-            System.out.println(0);
-            return;
-        }
+        for(int i = 0; i <= 999999; i++) {
+            String str = String.valueOf(i);
+            boolean check = true;
+            for(int j = 0; j < str.length(); j++) {
+                if(broken[str.charAt(j)-'0']) {
+                    check = false;
+                    break;
+                }
+            }
 
-        ArrayList<Integer> up = new ArrayList<>();
-        ArrayList<Integer> down = new ArrayList<>();
-
-        for(int i = 0; i < target.length(); i++) {
-            for(int j = 0; j <= 10; j++) {
-                int newNumber = target.charAt(i) - '0' + j;
+            if(check) {
+                int count = str.length() + Math.abs(N-i);
                 
-                if(newNumber > 9) newNumber -= 10;
-
-                if(!arraylist.contains(newNumber)) {
-                    up.add(newNumber);
-                    break;
-                }
-
-            }
+                answer = Math.min(answer, count);
+            } 
 
         }
 
-        for(int i = 0; i < target.length(); i++) {
-            for(int j = 0; j <= 10; j++) {
-                int newNumber = target.charAt(i) - '0' - j;
-                if(newNumber < 0) newNumber += 10;
-
-                if(!arraylist.contains(newNumber)) {
-                    down.add(newNumber);
-                    break;
-                }
-
-            }
-
-        }
-
-        int upNumber = 0;
-        int downNumber = 0;
-
-        for(int i = 0; i < up.size(); i++) {
-            upNumber += up.get(i) * Math.pow(10, up.size()-(i+1));
-            downNumber += down.get(i) * Math.pow(10, up.size()-(i+1));
-        }
-        int answer = Math.min(Math.abs(upNumber-Integer.parseInt(target)),Math.abs(downNumber-Integer.parseInt(target))) + target.length();
-        System.out.println(Math.min(Math.abs(100-Integer.parseInt(target)),answer));
-
+        System.out.println(answer);
 
     }
 }
