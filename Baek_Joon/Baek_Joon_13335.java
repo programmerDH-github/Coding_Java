64043bb112089f0daa65103117f4d58a0e25ed83
Donghun Lee <programmer_h@naver.com>
2025-02-24 23:55:29 +0900

    24 Feb

---
+++ Baek_Joon/Baek_Joon_13335.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_13335 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] n_w_L = br.readLine().split(" ");
+    }
+}
