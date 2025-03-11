49c4387c4f7d4111074f902151b28dd8e25ad88e
Donghun Lee <programmer_h@naver.com>
2025-03-11 23:56:02 +0900

    11 Mar

--- Baek_Joon/Baek_Joon_1713.java
+++ Baek_Joon/Baek_Joon_1713.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Collections;
 
 public class Baek_Joon_1713 {
     public static void main(String[] args) throws IOException {
@@ -11,22 +11,55 @@ public class Baek_Joon_1713 {
         int N = Integer.parseInt(br.readLine());
         int recommend = Integer.parseInt(br.readLine());
 
-        int[][] student = new int[recommend][3];
         String[] input = br.readLine().split(" ");
-        HashMap<Integer,int[]> map = new HashMap<>();
+        Student[] students = new Student[101];
+        ArrayList<Student> photo = new ArrayList<>();
         for(int i = 0; i < recommend; i++) {
-            int number = Integer.parseInt(input[i]);
-            if(!map.containsKey(number)) {
-                map.put(number, new int[]{1,i});
+            int num = Integer.parseInt(input[i]);
+            if(students[num] != null) {
+                students[num].like++;
             }
-            else {
-                int[] numbers = {map.get(number)[0]+1,i};
-                map.put(number,numbers);
+            else { 
+
+                Collections.sort(photo);
+
+                if(photo.size() == N) {                   
+
+                    students[photo.get(N-1).number] = null;
+                    photo.remove(N-1);
+
+                }
+
+                students[num] = new Student(num, 1, i);
+                photo.add(students[num]);
+
             }
         }
-        System.out.println(map);
-        ArrayList<Integer> arraylist = new ArrayList<>();
-        
-        
+
+        Collections.sort(photo, (o1,o2) -> o1.number - o2.number);
+
+        for(int i = 0; i < photo.size(); i++) {
+            System.out.print(photo.get(i).number + " ");
+        }
+
+    }
+
+    static class Student implements Comparable<Student>{
+        int number;
+        int like;
+        int idx;
+
+        public Student(int number, int like, int idx) {
+            this.number = number;
+            this.like = like;
+            this.idx = idx;
+        }
+
+        @Override
+        public int compareTo(Student s) {
+            if(this.like == s.like) return s.idx - this.idx;
+            else return s.like - this.like;
+        }
+
     }
 }
