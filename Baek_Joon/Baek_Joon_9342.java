5a9483cee2951103b9f20e4a48e0fd1bc7834ac6
Donghun Lee <programmer_h@naver.com>
2025-05-08 00:11:41 +0900

    8 May

--- Baek_Joon/Baek_Joon_9342.java
+++ Baek_Joon/Baek_Joon_9342.java
@@ -17,12 +17,57 @@ public class Baek_Joon_9342 {
                 continue;
             }
 
+            int count = 0;
             for(int j = 0; j < input.length(); j++) {
-                if(j == 0) {
-                    if(input.charAt(j) != 'A' && input.charAt(j) != 'B' && input.charAt(j) != 'C' && input.charAt(j) != 'D' && input.charAt(j) != 'E' && input.charAt(j) != 'F') {
+                if((int)input.charAt(j) > 70) {
+                    sb.append("Good").append("\n");
+                    break;
+                }
+
+                if(count == 0) {
+                    if((int)input.charAt(j) > 70) {
+                        sb.append("Good").append("\n");
                         break;
                     }
+                    
+                    count++;
+
                 }
+                else if(count == 1) {
+                    if(input.charAt(j) != 'A' && input.charAt(j) != 'F') {
+                        sb.append("Good").append("\n");
+                        break;
+                    }
+                    
+                    if(input.charAt(j) == 'F') count++;
+
+                }
+                else if(count == 2) {
+                    if(input.charAt(j) != 'F' && input.charAt(j) != 'C') {
+                        sb.append("Good").append("\n");
+                        break;
+                    }
+
+                    if(input.charAt(j) == 'C') count++;
+
+                }
+                else if(count == 3) {
+                    if(input.charAt(j) != 'C' && (int)input.charAt(j) > 70) {
+                        sb.append("Good").append("\n");
+                        break;
+                    }
+
+                    if((int)input.charAt(j) != 'C') count++;
+
+                }
+                else {
+                    sb.append("Good").append("\n");
+                }
+
+                if(j == input.length()-1) {
+                    sb.append("Infected!").append("\n");
+                }
+                
             }
 
         }
