048c823804b3dd03d2e8d76c32e648b8554c583b
huny\donghun <programmer_h@naver.com>
2025-06-13 00:45:22 +0900

    13 Jun

--- Baek_Joon/Beak_Joon_2792.java
+++ Baek_Joon/Beak_Joon_2792.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.PriorityQueue;
 
 public class Beak_Joon_2792 {
     public static void main(String[] args) throws IOException {
@@ -10,15 +9,37 @@ public class Beak_Joon_2792 {
         int N = Integer.parseInt(N_M[0]);
         int M = Integer.parseInt(N_M[1]);
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((o1,o2) -> o2 - o1);
-        int sum = 0;
+        int[] jewels = new int[M];
+        int max = 0;
         for(int i = 0; i < M; i++) {
             String input = br.readLine();
-            pq.offer(Integer.parseInt(input));
-            sum += Integer.parseInt(input);            
+            jewels[i] = Integer.parseInt(input);
+            if(max < jewels[i]) max = jewels[i];           
         }
 
-        System.out.println(sum/N + 1);
+        int left = 1;
+        int right = max;
+        int answer = 0;
+        while(left <= right) {
+            int mid = (left+right)/2;
+
+            int count = 0;
+            for(int jewel : jewels) {
+                count += jewel / mid;
+                if(jewel % mid != 0) count++;
+            }
+
+            if(count <= N) {
+                answer = mid;
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+            
+        }
+
+        System.out.println(answer);
 
     }
 }
