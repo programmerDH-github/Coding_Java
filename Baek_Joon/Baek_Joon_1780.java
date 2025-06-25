6bd010fe553c7f05b599b7f5c7c27365369a11ed
huny\donghun <programmer_h@naver.com>
2025-06-26 08:18:39 +0900

    26 Jun

--- Baek_Joon/Baek_Joon_1780.java
+++ Baek_Joon/Baek_Joon_1780.java
@@ -4,12 +4,13 @@ import java.io.InputStreamReader;
 
 public class Baek_Joon_1780 {
     static int[] count;
+    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         count = new int[3];
 
-        int[][] arr = new int[N][N];
+        arr = new int[N][N];
         for(int i = 0; i < N; i++) {
             String[] input = br.readLine().split(" ");
             for(int j = 0; j < N; j++) {
@@ -17,28 +18,41 @@ public class Baek_Joon_1780 {
             }
         }
 
-
-
+        back(N,0, 0, arr[0][0]);
 
         for(int c : count) System.out.println(c);
         
     }
 
-    static void back(int n, int[][] arr, int first, int second, int target) {
-        if(n == 1) {
+    static void back(int n, int x, int y, int target) {
+        if(check(x, y, n, target)) {
             count[target+1] += 1;
             return;
         }
+        
+        n /= 3;
 
-        int value = arr[first][second];
-        for(int i = first; i < n; i++) {
-            for(int j = second; j < n; j++) {
-                if(value != arr[i][j]) {
-                    back(n/3, arr, 0,0,value);
-                }                
+        back(n, x, y, arr[x][y]);
+        back(n, x, y+n, arr[x][y+n]);
+        back(n, x, y+2*n, arr[x][y+2*n]);
+
+        back(n, x+n, y, arr[x+n][y]);
+        back(n, x+n, y+n, arr[x+n][y+n]);
+        back(n, x+n, y+2*n, arr[x+n][y+2*n]);
+
+        back(n, x+2*n, y, arr[x+2*n][y]);
+        back(n, x+2*n, y+n, arr[x+2*n][y+n]);
+        back(n, x+2*n, y+2*n, arr[x+2*n][y+2*n]);
+
+    }
+
+    static boolean check(int x, int y, int n, int target) {
+        for(int i = x; i < x+n; i++) {
+            for(int j = y; j < y+n; j++) {
+                if(target != arr[i][j]) return false;
             }
         }
-
+        return true;
     }
 
 }
