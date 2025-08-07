60ccaeff52a3444c82aba426a5f667d35db70b1d
huny\donghun <programmer_h@naver.com>
2025-08-07 23:59:16 +0900

    7 Aug

--- Baek_Joon/Baek_Joon_1141.java
+++ Baek_Joon/Baek_Joon_1141.java
@@ -1,11 +1,51 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
 
 public class Baek_Joon_1141 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        System.out.println("aba".contains("ab"));
+
+        String[] X = new String[N];
+        for(int i = 0; i < N; i++) {
+            X[i] = br.readLine();
+        }
+
+        Arrays.sort(X, new Comparator<String>() {
+            @Override
+            public int compare(String s1, String s2) {
+                return s2.length() - s1.length();
+            }
+        });
+
+        HashSet<String> set = new HashSet<>();
+        
+        for(int i = 0; i < X.length; i++) {
+            if(set.size() == 0) {
+                set.add(X[i]);
+                continue;
+            }
+            
+            boolean check = false;
+            for(String str : set) {
+                if(str.indexOf(X[i]) == 0) {
+                    check = true;
+                    break;
+                }
+            }
+
+            if(!check) {
+                set.add(X[i]);
+            }
+
+        }
+
+        System.out.println(set.size());
+        
     }
 }
--- Baek_Joon/Baek_Joon_1373.java
+++ Baek_Joon/Baek_Joon_1373.java
@@ -1,15 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 
 public class Baek_Joon_1373 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
 
-        long N = Long.parseLong(input,2);
-        
-        System.out.println(Long.toString(N,8));
+        BigInteger N = new BigInteger(input,2);        
+        System.out.println(N.toString(8));
 
     }
 }
