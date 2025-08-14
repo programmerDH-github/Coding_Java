27cc311411f47d24907430dba0e48de4477c813a
huny\donghun <programmer_h@naver.com>
2025-08-15 05:40:05 +0900

    15 Aug

--- Baek_Joon/Baek_Joon_1342.java
+++ Baek_Joon/Baek_Joon_1342.java
@@ -1,19 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 
 public class Baek_Joon_1342 {
+    static int[] alphabet = new int[27];
+    static int count = 0;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String S = br.readLine();
-        HashMap<Character,Integer> map = new HashMap<>();
 
-        for(int i = 0 ; i < S.length(); i++) {
-            map.put(S.charAt(i),map.getOrDefault(S.charAt(i), 0) + 1);
+        for(int i = 0; i < S.length(); i++) {
+            alphabet[S.charAt(i) - 'a']++;
         }
+        
+        dfs(S,0,"");
 
-        System.out.println(map);
+        System.out.println(count);
 
     }
+
+    static void dfs(String s, int index, String temp) {
+        if(index == s.length()) {
+            count++;
+            return;
+        }
+
+        
+
+        for(int i = 0; i < 27; i++) {
+            if(alphabet[i] == 0) continue;
+
+            if(temp.length() == 0 || temp.charAt(temp.length()-1) != (char)('a'+i)) {
+                alphabet[i]--;
+                dfs(s,index+1,temp+(char)('a'+i));
+                alphabet[i]++;
+            }
+        }
+
+
+    }
+
 }
--- Baek_Joon/Baek_Joon_2798.java
+++ Baek_Joon/Baek_Joon_2798.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Baek_Joon_2798 {
     public static void main(String[] args) throws IOException {
@@ -8,5 +9,34 @@ public class Baek_Joon_2798 {
         String[] N_M = br.readLine().split(" ");
         int N = Integer.parseInt(N_M[0]);
         int M = Integer.parseInt(N_M[1]);
+
+        String[] input = br.readLine().split(" ");
+        int[] card = new int[N];
+        for(int i = 0; i < N; i++) {
+            card[i] = Integer.parseInt(input[i]);
+        }
+
+        Arrays.sort(card);
+
+        int answer = 0;
+        for(int i = 0; i < N; i++) {
+
+            if(card[i] >= M) break;
+
+            for(int j = i+1; j < N; j++) {
+
+                if(card[i]+card[j] >= M) break;
+
+                for(int k = j+1; k < N; k++) {
+
+                    if(card[i]+card[j]+card[k] > M) break;
+                    else if(answer < card[i]+card[j]+card[k]) answer = card[i]+card[j]+card[k];                    
+
+                }
+            }
+        }
+
+        System.out.println(answer);
+
     }
 }
