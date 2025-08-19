2dea652a5b52ac1c3545d931a90746663ba2d615
huny\donghun <programmer_h@naver.com>
2025-08-19 23:58:00 +0900

    19 Aug

---
+++ Baek_Joon/Baek_Joon_4307.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baek_Joon_4307 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < T; i++) {
+            String[] l_n = br.readLine().split(" ");
+            int l = Integer.parseInt(l_n[0]);
+            int n = Integer.parseInt(l_n[1]);
+
+
+            int[] ant = new int[n];
+            for(int j = 0; j < n; j++) {
+                ant[j] = Integer.parseInt(br.readLine());
+            }
+
+            Arrays.sort(ant);
+
+            System.out.println();
+            for(int j = 0; j < n; j++) System.out.print(ant[j]+" ");
+            
+
+
+        }
+
+    }
+}
