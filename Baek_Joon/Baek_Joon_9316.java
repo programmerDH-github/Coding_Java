491192f64812c8974afef8b940a58c9fb20d5014
huny\donghun <programmer_h@naver.com>
2025-08-31 23:43:22 +0900

    31 Aug

---
+++ Baek_Joon/Baek_Joon_9316.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_9316 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for(int i = 0; i < N; i++) System.out.println("Hello World, Judge "+(i+1)+"!");
+
+    }
+}
