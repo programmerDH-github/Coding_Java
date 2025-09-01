a68ff924e9a3d8b8abad3b61d1ff009f689baeb7
huny\donghun <programmer_h@naver.com>
2025-09-01 22:52:50 +0900

    1 Sep

---
+++ Baek_Joon/Baek_Joon_10101.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_10101 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+
+        if(a == 60 && b == 60 && c == 60) System.out.println("Equilateral");
+        else if(a+b+c == 180 && (a == b || b == c || a == c)) System.out.println("Isosceles");
+        else if(a+b+c == 180) System.out.println("Scalene");
+        else System.out.println("Error");
+
+    }
+}
