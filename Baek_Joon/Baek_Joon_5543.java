b81f4f8f1aa538610f9a2ab9c9fea56a251144d4
huny\donghun <programmer_h@naver.com>
2025-09-03 23:59:56 +0900

    3 Sep

---
+++ Baek_Joon/Baek_Joon_5543.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Baek_Joon_5543 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] hamburger = new int[3];
+        for(int i = 0; i < 3; i++) hamburger[i] = Integer.parseInt(br.readLine());
+        int[] drink = new int[2];
+        for(int i = 0; i < 2; i++) drink[i] = Integer.parseInt(br.readLine());
+
+        Arrays.sort(hamburger);
+        
+
+    }
+}
