9af4002d46a011d20f5c63247241e23e802cb17e
huny\donghun <programmer_h@naver.com>
2025-09-30 00:03:42 +0900

    30 Sep

--- Baek_Joon/Baek_Joon_11557.java
+++ Baek_Joon/Baek_Joon_11557.java
@@ -5,14 +5,31 @@ import java.io.InputStreamReader;
 public class Baek_Joon_11557 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
 
         for(int i = 0; i < T; i++) {
             int N = Integer.parseInt(br.readLine());
             int max = 0;
             String name = "";
+
+            for(int j = 0; j < N; j++) {
+                String[] input = br.readLine().split(" ");
+                String university = input[0];
+                int drink = Integer.parseInt(input[1]);
+
+                if(max < drink) {
+                    max = drink;
+                    name = university;
+                }
+
+            }
+            
+            sb.append(name).append("\n");
             
         }        
+
+        System.out.println(sb);
 
     }
 }
