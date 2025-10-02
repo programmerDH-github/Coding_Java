fd818a32903b764b54626ef5b74d4efdf0085fdc
huny\donghun <programmer_h@naver.com>
2025-10-02 23:44:06 +0900

    2 Oct

--- Baek_Joon/Baek_Joon_11947.java
+++ Baek_Joon/Baek_Joon_11947.java
@@ -5,24 +5,41 @@ import java.io.InputStreamReader;
 public class Baek_Joon_11947 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
         
-        long[] planet = new long[n];
-        String[] input = br.readLine().split(" ");
-        for(int i = 0; i < n; i++) planet[i] = Long.parseLong(input[i]);
+        for(int i = 0; i < T; i++) {
+            String input = br.readLine();
+            int N = Integer.parseInt(input);
 
-        long speed = Integer.MIN_VALUE;
-        for(int i = n-1; i >= 0; i--) {
-            if(planet[i] > speed) {
-                speed = planet[i];
+            
+            String digits = (1+"0".repeat(input.length()-1));
+            long mid = 5 * Long.parseLong(digits);
+            
+
+            if(N >= mid) {
+                String fn = "";
+                String midStr = mid + "";
+                for(int j = 0; j < midStr.length(); j++) {
+                    fn += 9 - (midStr.charAt(j) - '0');
+                }
+
+                sb.append(mid*Long.parseLong(fn)).append("\n");
+
             }
             else {
-                if(speed % planet[i] != 0) speed = ((speed / planet[i]) + 1) * planet[i];
+                String fn = "";
+                for(int j = 0; j < input.length(); j++) {
+                    fn += 9 - (input.charAt(j) - '0');
+                }
+
+                sb.append(N*Long.parseLong(fn)).append("\n");
+            
             }
 
-        } 
-
-        System.out.println(speed);
+        }
         
+        System.out.println(sb);
+
     }
 }
---
+++ Baek_Joon/Baek_Joon_17451.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Baek_Joon_17451 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        
+        long[] planet = new long[n];
+        String[] input = br.readLine().split(" ");
+        for(int i = 0; i < n; i++) planet[i] = Long.parseLong(input[i]);
+
+        long speed = Integer.MIN_VALUE;
+        for(int i = n-1; i >= 0; i--) {
+            if(planet[i] > speed) {
+                speed = planet[i];
+            }
+            else {
+                if(speed % planet[i] != 0) speed = ((speed / planet[i]) + 1) * planet[i];
+            }
+
+        } 
+
+        System.out.println(speed);
+        
+    }
+}
