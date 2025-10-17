64c1ba2c5d506cac4b6d57ef7ba5e16372f04bea
huny\donghun <programmer_h@naver.com>
2025-10-17 15:07:23 +0900

    17 Oct

--- Baek_Joon/Baek_Joon_23351.java
+++ Baek_Joon/Baek_Joon_23351.java
@@ -1,9 +1,34 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Baek_Joon_23351 {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int K = sc.nextInt();
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for(int i = 0; i < N; i++) {
+            pq.offer(K);
+        }
+
+        int water = 0;        
+        int day = 1;
+        while(pq.peek()-water > 0) {
+            
+            int watering = 0;
+            while(watering < A) {
+                pq.offer(pq.poll()+B);
+                watering++;
+            }
+
+            water++;
+            day++;
+        }
+        
+        System.out.println(day-1);
+
     }
 }
