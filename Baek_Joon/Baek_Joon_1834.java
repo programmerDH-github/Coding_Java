ebd7f37e07852da903d549a645614b2aeccc12b9
huny\donghun <programmer_h@naver.com>
2025-11-11 00:37:41 +0900

    11 Nov

--- Baek_Joon/Baek_Joon_1834.java
+++ Baek_Joon/Baek_Joon_1834.java
@@ -1,16 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 
 public class Baek_Joon_1834 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
 
-        while(true) {
-            
+        long answer = 0;
+
+        long remainder = 1;
+        while(N > remainder) {
+            answer += N*remainder + remainder;
+            remainder++;
         }
+
+        System.out.println(answer);
 
     }
 }
