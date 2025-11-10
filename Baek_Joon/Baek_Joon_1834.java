cbf7075cf1a461ac964883bfc3f18a7b8ba4c9ea
huny\donghun <programmer_h@naver.com>
2025-11-10 23:59:20 +0900

    10 Nov

---
+++ Baek_Joon/Baek_Joon_1834.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+
+public class Baek_Joon_1834 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        while(true) {
+            
+        }
+
+    }
+}
