1beb548f9b5d27f462db9539e868ea7cde4e8f2a
huny\donghun <programmer_h@naver.com>
2025-11-14 00:13:48 +0900

    14 Nov

--- Baek_Joon/Baek_Joon_11719.java
+++ Baek_Joon/Baek_Joon_11719.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Baek_Joon_11719 {
-    public static void main(String[] args) {
-        
+    public static void main(String[] args) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNext()) {
+            sb.append(sc.nextLine()).append("\n");
+        }
+
+        System.out.println(sb);
+
     }
 }
